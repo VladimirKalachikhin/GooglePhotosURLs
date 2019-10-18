4d5396a528270f8d80a089971f0535bfd8eab6f3
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2019-10-19 01:03:10 +0300

    let's go

---
+++ GooglePhotosURLs.php
@@ -0,0 +1,51 @@
+<?php
+
+$url = 'https://photos.app.goo.gl/ieZq8sHioiytnx6y6';
+$albumsPhotos=GooglePhotosURLs($url);
+echo "Album's Photos:<pre>"; print_r($albumsPhotos); echo "</pre>";
+
+function GooglePhotosURLs($albumURL,$width=NULL,$height=NULL,$urlsOnly=FALSE) {
+/* Get the GooglePhoto permanent album url, and
+return array of <img ...> strings of it photos, with permanent url's too.
+If no $width $height params - return full size images.
+If any of $width or $height params == 0 - return Google preview size images.
+If present $width or $height params - return scaled by Google to this size images.
+If present $width and $height params - return scaled by Google to min of this sizes images.
+
+If not album - not return. If empty album - empty array return.
+*/
+$needle = 'https://lh3';
+$albumsPhotos = array();
+$googletail = '';
+
+if( $album = fopen($albumURL,'r')) {
+	while (($buffer = fgets($album, 4096)) !== FALSE) {
+		if(($buffer=stristr($buffer,$needle)) === FALSE) continue; 	// если это не строка с требуемым url - проехали
+		$buffer = explode(',',$buffer);
+		if(count($buffer)!=10) continue; 	// требуемая строка - 10 значений, раздёлённых запятыми
+		$buffer[0]=trim($buffer[0],'"');
+		/* итак, первый элемент - url, второй и третий - width height, десятый - размер в байтах */
+		if($width===NULL AND $height===NULL) {
+			$googletail='=w'.$buffer[1].'-h'.$buffer[2]; 	// исходный размер
+		}
+		elseif($width AND $height) {
+			$googletail='=w'.$width.'-h'.$height; 		// приведение к меньшему из указанных размеров
+		}
+		elseif($width) {
+			$googletail='=w'.$width; 		// приведение к ширине
+		}
+		elseif($height) {
+			$googletail='=-h'.$height; 		// приведение к высоте
+		}
+		if($urlsOnly) $albumsPhotos[] = $buffer[0].$googletail;
+		else $albumsPhotos[] = '<img src="'.$buffer[0].$googletail.'>'."\n";
+	}
+	if (!feof($album)) {
+		echo "Error: unexpected fgets() fail, may be not all images presents\n";
+	}
+	fclose($album);
+	return $albumsPhotos;
+}
+} // end function GooglePhotosURLs
+?>
+
